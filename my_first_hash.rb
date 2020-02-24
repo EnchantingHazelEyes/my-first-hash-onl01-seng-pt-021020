f6a2337a64e64fa1f752e2b99611f084f96be263
Samantha Barros <samantha.barros95@gmail.com>
2020-02-24 02:49:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,6 +38,6 @@ shipping_manifest["gun powder"] = 4 # add 4 gun powder to the shipping_manifest 
 
 
 
-  puts shipping_manifest[] # return the shipping_manifest hash below
+  return shipping_manifest # return the shipping_manifest hash below
 
 end
